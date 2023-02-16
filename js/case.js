b8f418c7bc6cb8b4e1f2cd343cb382043e760e2a
sajim-khan <shossain1427@gmail.com>
2023-02-16 20:41:54 +0600

    added update case number and case pricing

--- js/case.js
+++ js/case.js
@@ -1,8 +1,36 @@
-document.getElementById('btn-case-plus').addEventListener('click', function() {
+/*
+    1. add event listener
+    2. get the value inside the case number field (input field)
+    3. convert the number to an integer
+    4. calculate the new case number
+    5. set the value to the new case field
+*/
+function updateCaseNumber(isIncrease){
     const caseNumberField = document.getElementById('case-number-field');
     const caseNumberString = caseNumberField.value;
     const previousCaseNumber = parseInt(caseNumberString);
     
-    const newCaseNumber = previousCaseNumber + 1;
+    let newCaseNumber ;
+    
+    if(isIncrease === true) {
+        newCaseNumber = previousCaseNumber + 1;
+    } else {
+        newCaseNumber = previousCaseNumber - 1;
+    }
+    
     caseNumberField.value = newCaseNumber;
+    
+    return newCaseNumber;
+}
+
+document.getElementById('btn-case-plus').addEventListener('click', function() {
+    const newCaseNumber = updateCaseNumber(true);
+    
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-price-total');
+    caseTotalElement.innerText = caseTotalPrice;
+});
+
+document.getElementById('btn-case-minus').addEventListener('click', function() {
+    updateCaseNumber(false);
 });
