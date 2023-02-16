fa1517f63032ab690589890b12b3d99c6a2697bf
sajim-khan <shossain1427@gmail.com>
2023-02-16 23:52:35 +0600

    added function in utilities file

--- js/phone.js
+++ js/phone.js
@@ -1,18 +1,29 @@
-document.getElementById('btn-phone-plus').addEventListener('click', function() {
+function updatePhoneNumber(newPhoneNumber) {
     const phoneNumberField = document.getElementById('phone-number-field');
     const phoneNumberString = phoneNumberField.value;
     const previousPhoneNumber = parseInt(phoneNumberString);
     
-    const newPhoneNumber = previousPhoneNumber + 1;
+    let newPhoneNumber ;
+    
+    if(isIncrease === true) {
+        newPhoneNumber = previousPhoneNumber + 1;
+    } else {
+        newPhoneNumber = previousPhoneNumber - 1;
+    }
+    
     phoneNumberField.value = newPhoneNumber;
+    
+    return newPhoneNumber;
+    
+    
+    
+}
+
+document.getElementById('btn-phone-plus').addEventListener('click', function() {
+    
 })
 
 
 document.getElementById('btn-phone-minus').addEventListener('click', function() {
-    const phoneNumberField = document.getElementById('phone-number-field');
-    const phoneNumberString = phoneNumberField.value;
-    const previousPhoneNumber = parseInt(phoneNumberString);
-    
-    const newPhoneNumber = previousPhoneNumber - 1;
-    phoneNumberField.value = newPhoneNumber;
+
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,17 @@
-console.log("dshf")
+function updateCaseNumber(isIncrease){
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
+    
+    let newCaseNumber ;
+    
+    if(isIncrease === true) {
+        newCaseNumber = previousCaseNumber + 1;
+    } else {
+        newCaseNumber = previousCaseNumber - 1;
+    }
+    
+    caseNumberField.value = newCaseNumber;
+    
+    return newCaseNumber;
+}
