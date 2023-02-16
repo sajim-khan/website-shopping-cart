48671e1c366565319f10b0952241e2579ec614de
sajim-khan <shossain1427@gmail.com>
2023-02-16 23:46:12 +0600

    added utilities file

---
+++ js/phone.js
@@ -0,0 +1,18 @@
+document.getElementById('btn-phone-plus').addEventListener('click', function() {
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberString = phoneNumberField.value;
+    const previousPhoneNumber = parseInt(phoneNumberString);
+    
+    const newPhoneNumber = previousPhoneNumber + 1;
+    phoneNumberField.value = newPhoneNumber;
+})
+
+
+document.getElementById('btn-phone-minus').addEventListener('click', function() {
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberString = phoneNumberField.value;
+    const previousPhoneNumber = parseInt(phoneNumberString);
+    
+    const newPhoneNumber = previousPhoneNumber - 1;
+    phoneNumberField.value = newPhoneNumber;
+})
