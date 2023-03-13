b20c5b983ea461c797c2ef50479bc0e4a0bdc1a8
sajim-khan <shossain1427@gmail.com>
2023-03-13 23:34:52 +0600

    added few code

--- js/utilities.js
+++ js/utilities.js
@@ -1,20 +1,4 @@
-// function updateCaseNumber(isIncrease){
-//     const caseNumberField = document.getElementById('case-number-field');
-//     const caseNumberString = caseNumberField.value;
-//     const previousCaseNumber = parseInt(caseNumberString);
-    
-//     let newCaseNumber ;
-    
-//     if(isIncrease === true) {
-//         newCaseNumber = previousCaseNumber + 1;
-//     } else {
-//         newCaseNumber = previousCaseNumber - 1;
-//     }
-    
-//     caseNumberField.value = newCaseNumber;
-    
-//     return newCaseNumber;
-// }
+
 
 
 function getTextElementValueById (elementId) {
@@ -27,6 +11,7 @@ function getTextElementValueById (elementId) {
 
 
 function setTextElementValueById(elementId, value) {
+    console.count()
     const subTotalElement = document.getElementById(elementId);
     subTotalElement.innerText = value;
 }
